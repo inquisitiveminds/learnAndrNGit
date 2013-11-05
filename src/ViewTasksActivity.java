a4ec5728c092f047efaa5057ac4b7fb65244cd3d
inquisitiveminds <inquisitivem@server.fake>
2013-11-04 22:30:15 -0500

    Initial Commit

---
+++ src/ViewTasksActivity.java
@@ -0,0 +1,18 @@
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class ViewTasksActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+  		super.onCreate(savedInstanceState);
+  		setContentView(R.layout.main);
+	}
+	
+
+}
